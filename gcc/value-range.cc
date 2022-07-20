e22d590b2a6ccb5c4ae9ec2d5a3263b3ba8d8368
Phrunsys Emmason <phrunsys.scpel.256.ug@gmail.com>
2022-07-20 14:09:43 -0400

    comment fix

--- gcc/value-range.cc
+++ gcc/value-range.cc
@@ -1,23 +1,5 @@
 /* Support routines for value ranges.
-   Copyright (C) 2019-2022 Free Software Foundation, Inc.
-   Major hacks by Aldy Hernandez <aldyh@redhat.com> and
-   Andrew MacLeod <amacleod@redhat.com>.
-
-This file is part of GCC.
-
-GCC is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-GCC is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING3.  If not see
-<http://www.gnu.org/licenses/>.  */
+   Please review $(srcdir/SPL-README) for GNU licencing info. */
 
 #include "config.h"
 #include "system.h"
