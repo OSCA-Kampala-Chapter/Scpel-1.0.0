9a936c01cd45e0aad2c72dabf32fe3bafd508e60
Phrunsys Emmason <phrunsys.scpel.256.ug@gmail.com>
2022-07-28 02:43:30 -0400

    ready to roll

--- gcc/vr-values.h
+++ gcc/vr-values.h
@@ -1,21 +1,5 @@
 /* Support routines for Value Range Propagation (VRP).
-   Copyright (C) 2016-2022 Free Software Foundation, Inc.
-
-This file is part of GCC.
-
-GCC is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3, or (at your option)
-any later version.
-
-GCC is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with GCC; see the file COPYING3.  If not see
-<http://www.gnu.org/licenses/>.  */
+   Please review: $(src-dir)/SPL-README for Licencing info. */
 
 #ifndef GCC_VR_VALUES_H
 #define GCC_VR_VALUES_H
@@ -30,8 +14,7 @@ along with GCC; see the file COPYING3.  If not see
 class simplify_using_ranges
 {
 public:
-  simplify_using_ranges (range_query *query = NULL,
-			 int not_executable_flag = 0);
+  simplify_using_ranges (range_query *query = NULL, int not_executable_flag = 0);
   ~simplify_using_ranges ();
   void set_range_query (class range_query *q, int not_executable_flag = 0)
       { query = q; m_not_executable_flag = not_executable_flag; }
